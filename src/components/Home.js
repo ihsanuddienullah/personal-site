0b7a7518b4453297a0763c6d570aa8707554c136
ihsanuddienullah <ihsanuddienullah@gmail.com>
2021-04-16 22:17:01 +0700

    fix icon

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
-import { Jumbotron, Button, Container, Row, Col } from 'react-bootstrap';
-import homeImg from '../assets/img/home.svg';
+import React, { Component } from "react";
+import { Jumbotron, Button, Container, Row, Col } from "react-bootstrap";
+import homeImg from "../assets/img/home.svg";
 import { Animated } from "react-animated-css";
 
 export default class Home extends Component {
@@ -11,22 +11,20 @@ export default class Home extends Component {
                     <Container>
                         <Row className="align-items-center">
                             <Col xs={12} sm={6} md={6} lg={6} xl={6}>
-                                <h1>Oh, Hello World!</h1>
+                                <h1>Oh, Hello World!</h1>                                
+                                <p>I'm Muhammad Ihsanuddienullah</p>
+                                <p>- Software Engineer</p>
                                 <p>
-                                    My name is
-                                    </p>
-                                <p>
-                                    Muhammad Ihsanuddienullah
-                                    </p>
-                                <p>
-                                    I'm a Software Engineer
-                                    </p>
-                                <p>
-                                    <Button variant="primary" href="#about">More about me ...</Button>
+                                    <Button variant="primary" href="#about">
+                                        More about me ...
+                                    </Button>
                                 </p>
                             </Col>
                             <Col xs={12} sm={6} md={6} lg={6} xl={6}>
-                                <Animated animationIn="pulse" className="animate__infinite	infinite">
+                                <Animated
+                                    animationIn="pulse"
+                                    className="animate__infinite	infinite"
+                                >
                                     <img src={homeImg} alt="code" />
                                 </Animated>
                             </Col>
@@ -34,6 +32,6 @@ export default class Home extends Component {
                     </Container>
                 </Jumbotron>
             </div>
-        )
+        );
     }
 }
--- src/components/Works.js
+++ src/components/Works.js
@@ -1,13 +1,22 @@
-import React, { Component } from 'react';
-import { Jumbotron, Card, Container, Row, Col } from 'react-bootstrap';
-import worksImg from '../assets/img/works.svg';
-import worksImg1 from '../assets/img/works1.svg';
-import worksImg2 from '../assets/img/works2.svg';
-import worksImg3 from '../assets/img/works3.svg';
-import worksImg4 from '../assets/img/works4.svg';
-import worksImg5 from '../assets/img/works5.svg';
-import worksImg6 from '../assets/img/works6.svg';
-import ScrollAnimation from 'react-animate-on-scroll';
+import React, { Component } from "react";
+import { Jumbotron, Card, Container, Row, Col } from "react-bootstrap";
+import worksImg from "../assets/img/works.svg";
+import worksImg1 from "../assets/img/works1.svg";
+import worksImg2 from "../assets/img/works2.svg";
+import worksImg3 from "../assets/img/works3.svg";
+import worksImg4 from "../assets/img/works4.svg";
+import worksImg5 from "../assets/img/works5.svg";
+import worksImg6 from "../assets/img/works6.svg";
+import ScrollAnimation from "react-animate-on-scroll";
+import {
+    SiHtml5,
+    SiCss3,
+    SiJavascript,
+    SiPhp,
+    SiJava,
+    SiCplusplus,
+    SiReact,
+} from "react-icons/si";
 
 export default class Works extends Component {
     render() {
@@ -16,37 +25,43 @@ export default class Works extends Component {
                 <Jumbotron>
                     <Container>
                         <Row className="align-items-center">
-                            <Col id="worksIcon" xs={12} sm={6} md={6} lg={6} xl={6}>
-                                <ScrollAnimation animateIn="zoomIn" animateOnce="true">
+                            <Col
+                                id="worksIcon"
+                                xs={12}
+                                sm={6}
+                                md={6}
+                                lg={6}
+                                xl={6}
+                            >
+                                <ScrollAnimation
+                                    animateIn="zoomIn"
+                                    animateOnce="true"
+                                >
                                     <h1>Works</h1>
                                     <Row>
                                         <Col xs={6} sm={6} md={6} lg={6} xl={6}>
                                             <Card>
                                                 <Card.Body>
-                                                    <img src={worksImg1} alt="code" />
+                                                    <h1>
+                                                        <SiHtml5 />
+                                                    </h1>
+                                                    {/* <img
+                                                        src={worksImg1}
+                                                        alt="code"
+                                                    /> */}
                                                 </Card.Body>
                                             </Card>
                                         </Col>
                                         <Col xs={6} sm={6} md={6} lg={6} xl={6}>
                                             <Card>
                                                 <Card.Body>
-                                                    <img src={worksImg2} alt="code" />
-                                                </Card.Body>
-                                            </Card>
-                                        </Col>
-                                    </Row>
-                                    <Row>
-                                        <Col xs={6} sm={6} md={6} lg={6} xl={6}>
-                                            <Card>
-                                                <Card.Body>
-                                                    <img src={worksImg3} alt="code" />
-                                                </Card.Body>
-                                            </Card>
-                                        </Col>
-                                        <Col xs={6} sm={6} md={6} lg={6} xl={6}>
-                                            <Card>
-                                                <Card.Body>
-                                                    <img src={worksImg4} alt="code" />
+                                                    <h1>
+                                                        <SiCss3 />
+                                                    </h1>
+                                                    {/* <img
+                                                        src={worksImg2}
+                                                        alt="code"
+                                                    /> */}
                                                 </Card.Body>
                                             </Card>
                                         </Col>
@@ -55,22 +70,66 @@ export default class Works extends Component {
                                         <Col xs={6} sm={6} md={6} lg={6} xl={6}>
                                             <Card>
                                                 <Card.Body>
-                                                    <img src={worksImg5} alt="code" />
+                                                    <h1>
+                                                        <SiJavascript />
+                                                    </h1>
+                                                    {/* <img
+                                                        src={worksImg3}
+                                                        alt="code"
+                                                    /> */}
                                                 </Card.Body>
                                             </Card>
                                         </Col>
                                         <Col xs={6} sm={6} md={6} lg={6} xl={6}>
                                             <Card>
                                                 <Card.Body>
-                                                    <img src={worksImg6} alt="code" />
+                                                    <h1>
+                                                        <SiPhp />
+                                                    </h1>
+                                                    {/* <img
+                                                        src={worksImg4}
+                                                        alt="code"
+                                                    /> */}
+                                                </Card.Body>
+                                            </Card>
+                                        </Col>
+                                    </Row>
+                                    <Row>
+                                        <Col xs={6} sm={6} md={6} lg={6} xl={6}>
+                                            <Card>
+                                                <Card.Body>
+                                                    <h1>
+                                                        <SiJava />
+                                                    </h1>
+                                                    {/* <img
+                                                        src={worksImg5}
+                                                        alt="code"
+                                                    /> */}
+                                                </Card.Body>
+                                            </Card>
+                                        </Col>
+                                        <Col xs={6} sm={6} md={6} lg={6} xl={6}>
+                                            <Card>
+                                                <Card.Body>
+                                                    <h1>
+                                                        <SiCplusplus />
+                                                    </h1>
+                                                    {/*
+                                                    <img
+                                                        src={worksImg6}
+                                                        alt="code"
+                                                    /> */}
                                                 </Card.Body>
                                             </Card>
                                         </Col>
                                     </Row>
                                 </ScrollAnimation>
                             </Col>
-                            <Col >
-                                <ScrollAnimation animateIn="zoomIn" animateOnce="true">
+                            <Col>
+                                <ScrollAnimation
+                                    animateIn="zoomIn"
+                                    animateOnce="true"
+                                >
                                     <img src={worksImg} alt="code" />
                                 </ScrollAnimation>
                             </Col>
@@ -78,6 +137,6 @@ export default class Works extends Component {
                     </Container>
                 </Jumbotron>
             </div>
-        )
+        );
     }
 }
