c10d92d49d9054fabb29d3629840494b5e578850
ihsanuddienullah <muhammad.ihsanuddienullah@xapiens.id>
2021-08-21 17:23:52 +0700

    fix error unuserd var

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -2,13 +2,12 @@ import React, { Component } from "react";
 import Header from "../components/Header";
 import Home from "../components/Home";
 import About from "../components/About";
-import Works from "../components/Works";
+// import Works from "../components/Works";
 import Contact from "../components/Contact";
-import Portfolios from "../components/Portfolios";
-import Certificates from "../components/Certificates";
-import Quotes from "../components/Quotes"
+// import Portfolios from "../components/Portfolios";
+// import Certificates from "../components/Certificates";
+// import Quotes from "../components/Quotes"
 import Footer from "../components/Footer";
-
 
 export default class LandingPage extends Component {
     render() {
